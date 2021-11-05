988ba54d9cc6e354666b6576ff28006f91e41feb
Dexlysic <dexlysichd@gmail.com>
2021-11-05 18:39:41 -0500

    get_user_input defined to reduce repetiton

--- blockchain.py
+++ blockchain.py
@@ -6,17 +6,21 @@ def get_last_block():
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=["start"]):
+def add_value(transaction_amount, last_transaction=["block_start"]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-tx_amount = float(input("Please enter transaction amount: "))
+def get_user_input():
+    return float(input("Please enter transaction amount: "))
+
+
+tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = float(input("Please enter transaction amount: "))
+tx_amount = get_user_input()
 add_value(tx_amount, get_last_block())
 
-tx_amount = float(input("Please enter transaction amount: "))
+tx_amount = get_user_input()
 # Below is redundant use of keyword arguments (kwargs), simply for showing example of use.
 add_value(last_transaction=get_last_block(), transaction_amount=tx_amount)
 
